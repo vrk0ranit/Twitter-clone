e267c53a3cdff16b9ee223c6033bba3e36d028f0
vrk0ranit <ranitbag49@gmail.com>
2024-04-27 09:10:46 +0530

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express"
+import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import connectMongoDB from "./db/connectMongoDB.js"
+
+dotenv.config()
 
 const app = express()
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running");
+    connectMongoDB();
 })
 
 app.use('/api/auth', authRoutes)
