19371b915a5fee00f40750934464065c5f4487ea
vrk0ranit <ranitbag49@gmail.com>
2024-05-01 22:02:18 +0530

    finished update user controller

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-router.post("/update", protectRoute, updateUser);
+router.get("/update", protectRoute, updateUser);
 
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,15 @@ import usersRoutes from './routes/user.route.js'
 import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser"
 
+import {v2 as cloudinary} from 'cloudinary'
+
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.LOUDINARY_API_SECRET,
+})
 
 const app = express()
 app.use(express.json()) //to parse req.body
