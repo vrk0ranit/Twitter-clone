1ebb5c6ff2d9ddce7d01aa94a225ee0a3fc6b254
vrk0ranit <ranitbag49@gmail.com>
2024-05-01 21:19:00 +0530

    finish suggested user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,12 +55,32 @@ export const followUnfollowUser = async (req, res) => {
 	}
 };
 export const getSuggestedUsers = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+	try {
+		const userId = req.user._id;
+
+		const usersFollowedByMe = await User.findById(userId).select("following");
+
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{ $sample: { size: 10 } },
+		]);
+
+		// 1,2,3,4,5,6,
+		const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0, 4);
+
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		console.log("Error in getSuggestedUsers: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
+};
 export const updateUser = async (req, res) => {
     try {
         
